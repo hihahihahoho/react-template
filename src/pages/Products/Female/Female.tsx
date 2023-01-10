1373f2269ab6f5d9206bfcc6f27aa69c7245dc8f
Tung <hihahihahoho@gmail.com>
2023-01-11 00:00:46 +0700

    update

--- src/pages/Products/Female/Female.tsx
+++ src/pages/Products/Female/Female.tsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 const Female: React.FC = () => {
   return (
     <>
-      				<Link to={'../'}>Back</Link>
-      				<div>female</div>
+      <Link to={'../'}>Back</Link>
+      <div>female</div>
     </>
   );
 };
