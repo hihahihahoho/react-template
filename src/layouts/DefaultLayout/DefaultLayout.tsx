3aafaa1098929f452fc6c8d5e26f211fbba4f473
Tung <hihahihahoho@gmail.com>
2023-01-15 23:42:38 +0700

    update

--- src/layouts/DefaultLayout/DefaultLayout.tsx
+++ src/layouts/DefaultLayout/DefaultLayout.tsx
@@ -6,46 +6,46 @@ import Sidebar from '../sidebar/Sidebar';
 import styles from './DefaultLayout.module.scss';
 
 type Props = {
-  children: React.ReactElement;
+	children: React.ReactElement;
 };
 
 type CollapsedType = boolean;
 type SetCollapsedType = (collapsed: CollapsedType) => void;
 
 interface ContextType {
-  collapsed: CollapsedType;
-  setCollapsed: SetCollapsedType;
+	collapsed: CollapsedType;
+	setCollapsed: SetCollapsedType;
 }
 
 const SidebarCollapsed = createContext<ContextType | undefined>(undefined);
 
 const DefaultLayout: React.FC<Props> = ({ children }) => {
-  const [collapsed, setCollapsed] = useState<CollapsedType>(false);
-  return (
-    <>
-      <SidebarCollapsed.Provider value={{ collapsed, setCollapsed }}>
-        <Layout className={styles['side-bar']} style={{ height: '100vh' }} hasSider={true}>
-          <Sidebar />
-          <Layout className='site-layout'>
-            <Header style={{ padding: 0 }}>
-              {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                className: 'trigger',
-                onClick: () => setCollapsed(!collapsed),
-              })}
-            </Header>
-            <Content
-              style={{
-                margin: '24px 16px',
-                padding: 24,
-              }}
-            >
-              {children}
-            </Content>
-          </Layout>
-        </Layout>
-      </SidebarCollapsed.Provider>
-    </>
-  );
+	const [collapsed, setCollapsed] = useState<CollapsedType>(false);
+	return (
+		<>
+			<SidebarCollapsed.Provider value={{ collapsed, setCollapsed }}>
+				<Layout className={styles['side-bar']} style={{ height: '100vh' }} hasSider={true}>
+					<Sidebar />
+					<Layout className='site-layout'>
+						<Header style={{ padding: 0 }}>
+							{React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+								className: 'trigger',
+								onClick: () => setCollapsed(!collapsed),
+							})}
+						</Header>
+						<Content
+							style={{
+								margin: '24px 16px',
+								padding: 24,
+							}}
+						>
+							{children}
+						</Content>
+					</Layout>
+				</Layout>
+			</SidebarCollapsed.Provider>
+		</>
+	);
 };
 
 export default DefaultLayout;
