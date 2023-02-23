63912f4f2a99f27640a2d5dcc0b901d64b5ff04d
tungnt2 <tungnt2@vnpay.vn>
2023-02-23 19:47:06 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import './App.css';
 import RenderRouter from './routes/RenderRouter';
 
 export const CLIENT_ID = '70816812996-rouhd80qba9vvbu00vn2pd7oab8tnkrc.apps.googleusercontent.com';
-export const API_KEY = 'AIzaSyDZ0UhrwW13scAgcHCIIvQCatIBOHF8Y_o';
 export const FOLDER_ID = '1yHmyPb1Mr57E31ua1H2zL2bhOp_BASeM';
 export const SCOPE = 'https://www.googleapis.com/auth/drive';
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -13,7 +13,6 @@ const onFinish = (values: any) => {
 };
 
 export const CLIENT_ID = '70816812996-rouhd80qba9vvbu00vn2pd7oab8tnkrc.apps.googleusercontent.com';
-export const API_KEY = 'AIzaSyDZ0UhrwW13scAgcHCIIvQCatIBOHF8Y_o';
 export const FOLDER_ID = '1yHmyPb1Mr57E31ua1H2zL2bhOp_BASeM';
 export const SCOPE = 'https://www.googleapis.com/auth/drive';
 
