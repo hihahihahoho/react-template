6e5d1c4d3d9fc2401fa43584849ae0460caa35bb
tungnt2 <tungnt2@vnpay.vn>
2023-02-23 19:46:40 +0700

    update

--- src/components/GgDriveUploader.tsx
+++ src/components/GgDriveUploader.tsx
@@ -6,7 +6,6 @@ import { useStore } from 'zustand';
 import useThemeStore from '../states/useThemeStore';
 
 export const CLIENT_ID = '70816812996-rouhd80qba9vvbu00vn2pd7oab8tnkrc.apps.googleusercontent.com';
-export const API_KEY = 'AIzaSyDZ0UhrwW13scAgcHCIIvQCatIBOHF8Y_o';
 export const FOLDER_ID = '1yHmyPb1Mr57E31ua1H2zL2bhOp_BASeM';
 export const SCOPE = 'https://www.googleapis.com/auth/drive';
 
